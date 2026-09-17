84b0e41c9a0985c5edcffe62c9e1f4801dce936c
WBQ-quan <2713928010@qq.com>
2026-09-17 18:17:53 +0800

    Update convert_to_csv.py path

--- data/convert_to_csv.py
+++ data/convert_to_csv.py
@@ -5,6 +5,6 @@ file_path = r"D:\GlobalSuperstore\data_analysis_projects\superstore-analysisi\da
 df = pd.read_excel(file_path, sheet_name="Orders", engine="xlrd")
 
 # 转存 CSV 到 data 文件夹（同目录）
-df.to_csv(r"D:\GlobalSuperstore\data_analysis_projects\superstore-analysisi\data\global_superstore.csv", index=False, encoding="utf-8-sig")
+df.to_csv(r"D:\projectB-superstore-profit-dashboard\data\global_superstore.csv", index=False, encoding="utf-8-sig")
 
 print(f"转换成功！共读取 {df.shape[0]} 行，{df.shape[1]} 列。")
